bd7df673a96cdd85e6990acad0c972b35d6eca20
Alexander0212 <rotor02122004@gmail.com>
2023-05-07 15:37:16 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XML_перше_знайомство
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public bool Programmer { get; private set; }
+        public Employee(string name, int age, bool programmer)
+        {
+        Name = name;
+        Age = age;
+        Programmer = programmer;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+  
+
+
+}
